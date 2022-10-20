2b94f107a1dc214606c8ab9ad5300181ae77686e
Patricia Miguel <contacto@patriciaemiguel.com>
2022-10-20 19:16:57 -0300

    Refactored importsto improve readability.

--- ENG/run_tests.py
+++ ENG/run_tests.py
@@ -3,19 +3,19 @@
 #########################################
 
 import unittest
-import tests.tests_numbers as numbers
-import tests.tests_strings as strings
-import tests.tests_lists as lists
-import tests.tests_dictionaries as dictionaries
+from tests import tests_numbers
+from tests import tests_strings
+from tests import tests_lists
+from tests import tests_dictionaries
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # Comment out the lines related to topics you'd like to exclude from execution.
-suite.addTests(loader.loadTestsFromModule(numbers))
-suite.addTests(loader.loadTestsFromModule(strings))
-suite.addTests(loader.loadTestsFromModule(lists))
-suite.addTests(loader.loadTestsFromModule(dictionaries))
+suite.addTests(loader.loadTestsFromModule(tests_numbers))
+suite.addTests(loader.loadTestsFromModule(tests_strings))
+suite.addTests(loader.loadTestsFromModule(tests_lists))
+suite.addTests(loader.loadTestsFromModule(tests_dictionaries))
 
 runner = unittest.TextTestRunner(verbosity=2)
 result = runner.run(suite)
--- ESP/ejecutar_tests.py
+++ ESP/ejecutar_tests.py
@@ -3,20 +3,19 @@
 #######################################################
 
 import unittest
-import tests.tests_numeros as numeros
-import tests.tests_strings as strings
-import tests.tests_listas as listas
-import tests.tests_diccionarios as diccionarios
-
+from tests import tests_numeros
+from tests import tests_strings
+from tests import tests_listas
+from tests import tests_diccionarios
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # Comentar las líneas correspondiente a las áreas temáticas a excluir durante la ejecución.
-suite.addTests(loader.loadTestsFromModule(numeros))
-suite.addTests(loader.loadTestsFromModule(strings))
-suite.addTests(loader.loadTestsFromModule(listas))
-suite.addTests(loader.loadTestsFromModule(diccionarios))
+suite.addTests(loader.loadTestsFromModule(tests_numeros))
+suite.addTests(loader.loadTestsFromModule(tests_strings))
+suite.addTests(loader.loadTestsFromModule(tests_listas))
+suite.addTests(loader.loadTestsFromModule(tests_diccionarios))
 
 runner = unittest.TextTestRunner(verbosity=2)
 result = runner.run(suite)
